b4d18f06b69cfaba35088e57a7145c8f6f601ecb
xChristianTWM <omgitscristian@outlook.es>
2024-08-08 16:30:41 -0300

    Delete unnecessary files scss

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<script>
+{/* <script>
 // Función para alternar la visibilidad del menú en la barra de navegación
     function toggleMenu() {
         const menuBar = document.querySelector('.container-bar');
         menuBar.classList.toggle('active');
     }
-
-// Función para alternar la visibilidad del aside (filtros) en pantallas pequeñas
-    function toggleAside() {
-        const aside = document.querySelector('.left-column');
-        aside.classList.toggle('active');
-    }
-
-
-
-</script>
+</script> */}
