c31d682d19c7d5f816415f31e204edf73e8c88e8
xChristianTWM <omgitscristian@outlook.es>
2024-08-08 16:12:16 -0300

    Fix responsive and design

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 <script>
+// Función para alternar la visibilidad del menú en la barra de navegación
     function toggleMenu() {
         const menuBar = document.querySelector('.container-bar');
         menuBar.classList.toggle('active');
     }
+
+// Función para alternar la visibilidad del aside (filtros) en pantallas pequeñas
+    function toggleAside() {
+        const aside = document.querySelector('.left-column');
+        aside.classList.toggle('active');
+    }
+
+
+
 </script>
